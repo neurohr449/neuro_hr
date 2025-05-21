fbee6768e5d9a75c271e6ce6abf051a951aa8e08
selivants <selivants@gmail.com>
2025-05-21 10:18:07 +0300

    test time

--- functions.py
+++ functions.py
@@ -295,9 +295,9 @@ async def check_empty_cells(sheet_id: str) -> InlineKeyboardMarkup | None:
             if col_idx < 26:  # B-Z (0-25)
                 col_letter = chr(66 + col_idx)  # B=66, ..., Z=90
             elif col_idx < 52:  # AA-AZ (26-51)
-                col_letter = 'A' + chr(65 + (col_idx - 26))  # AA=65+0, ..., AZ=65+25
+                col_letter = 'A' + chr(66 + (col_idx - 26))  # AA=65+0, ..., AZ=65+25
             else:  # BA-BS (52-69)
-                col_letter = 'B' + chr(65 + (col_idx - 52))  # BA=65+0, ..., BS=65+17 (S=83)
+                col_letter = 'B' + chr(66 + (col_idx - 52))  # BA=65+0, ..., BS=65+17 (S=83)
             
             has_empty = False
             
